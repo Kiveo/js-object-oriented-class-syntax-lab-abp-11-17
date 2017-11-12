5e00a44fba87fc4a1f24e9be67541baa670c39fd
Maverick Tesini <mavericktesini@gmail.com>
2017-11-12 17:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ class BoardMember {
     this.homeState= homeState;
     this.training=training;
   }
+
   //methods
   veto() {
     return "No, I must disagree";
   }
+
   approve() {
     return "You can do that!";
   }
+
   doCharity() {
     return "I like to help people.";
   }
+
   releasePressStatement() {
     return "You will see great things from Scuber.";
   }
