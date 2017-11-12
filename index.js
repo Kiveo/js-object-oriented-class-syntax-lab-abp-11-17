40e6c7ae226216953a6cc756ce7cfb82b435f228
Maverick Tesini <mavericktesini@gmail.com>
2017-11-12 17:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 class BoardMember {
   constructor(name, homeState, training) {
-    this.homeState= homeState;
     this.name= name;
+    this.homeState= homeState;
     this.training=training;
   }
 
