1b669bf5e4ad56775e7e5e16e1d09b3b946f0213
Maverick Tesini <mavericktesini@gmail.com>
2017-11-12 17:40:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class BoardMember {
   releasePressStatement() {
     return "You will see great things from Scuber.";
   }
-  debugger
+  debugger;
   sayHi() {
     return "Hi, my name is "+ name + ". I am from " + homeState + ", and I was trained in " + training + ".";
   }
